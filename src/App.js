5096e8af80b1109afe2c2eceb78c43a3e3bc5f8c
VahanAper <asryanvahandev@gmail.com>
2017-06-20 17:00:08 +0400

    some basic routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-const App = () => <h1>Hello World!!!</h1>
+const Home = (props) => {
+  console.log('props', props);
+  return <h1>Home</h1>;
+}
+
+const App = () => (
+  <Router>
+    <div>
+      <Route exact path="/" component={Home} />
+      {/*<Route path="/about" render={() => <h1>About</h1>} />*/}
+      <Route
+        path="/about"
+        children={({match}) => match && <h2>About</h2>}
+      />
+    </div>
+  </Router>
+);
 
 export default App;
