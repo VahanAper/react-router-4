33888841ef96dc8ae2a4b446a740050bd7d7f296
VahanAper <asryanvahandev@gmail.com>
2017-06-21 12:11:40 +0400

    render multiple Components for the same route

--- src/App.js
+++ src/App.js
@@ -3,34 +3,45 @@ import {
   BrowserRouter as Router,
   Route,
   Link,
-  Switch,
 } from 'react-router-dom';
 
 import './App.css';
 
 const Links = () => (
   <nav>
-    <Link to="/">Home</Link>
+    <Link to="/home">Home</Link>
     <Link to="/about">About</Link>
-    <Link to="/contact">Contact</Link>
   </nav>
+);
+
+const Header = ({ match }) => (
+  <div className="header">
+    <Route
+      path="/:page"
+      render={({ match }) =>
+        <h1>{match.params.page} HEADER</h1>
+      }
+    />
+  </div>
+);
+
+const Content = ({ match }) => (
+  <div className="content">
+    <Route
+      path="/:page"
+      render={({ match }) =>
+        <p>{match.params.page} CONTENT</p>
+      }
+    />
+  </div>
 );
 
 const App = () => (
   <Router>
     <div>
       <Links />
-      <Switch>
-        <Route exact path="/" render={() => <h1>Home</h1>}/>
-        <Route path="/about" render={() => <h1>About</h1>}/>
-        <Route path="/contact" render={() => <h1>Contact</h1>}/>
-        <Route
-          path="/:itemid"
-          render={({ match }) =>
-          <h1>Item: {match.params.itemid}</h1>
-        }
-        />
-      </Switch>
+      <Header />
+      <Content />
     </div>
   </Router>
 );
